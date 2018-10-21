ea2f9c5554ca71694f65bfa2cffd4934f986a7c3
Mateus Gabi <mateusgabimoreira@hotmail.com>
2018-10-21 16:12:49 -0400

    feat(character): screen id dynamic

--- frontend/src/Screens/Character.jsx
+++ frontend/src/Screens/Character.jsx
@@ -1,23 +1,58 @@
 import React from 'react';
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
-const CharacterScreen = () => (
-  <div className="CharacterScreen__wrapper">
-    <div className="CharacterScreen__firstLine">
-      <span>close</span>
-    </div>
-    <div className="CharacterScreen__content">
-      <div className="CharacterScreen__content--text">
-        <h1>A-Bomb (HAS)</h1>
-        <p>Rick Jones has been Hulk's best bud since day one, but now he's more than a friend...he's a teammate! Transformed by a Gamma energy explosion, A-Bomb's thick, armored skin is just as strong and powerful as it is blue. And when he curls into action, he uses it like a giant bowling ball of destruction!</p>
-      </div>
-      <div>
-          <div className="CharacterScreen__image"></div>
-      </div>
-    </div>
-    <div className="CharacterScreen__bottom">
-      <p>Data provides by Marvel API</p>
-    </div>
-  </div>
+const CharacterScreen = ({match}) => (
+  <Query
+    variables={{ id:  match.params.id }}
+    query={gql`
+      query character($id: String) {
+        character(id: $id) {
+      		id,
+      		name,
+      		description,
+      		thumbnail {
+      			url: complete
+      		}
+      	}
+      }
+    `}
+  >
+  {
+    ({ loading, error, data }) => {
+      if (loading) {
+        return <div>carregando...</div>
+      }
+
+      if (error) {
+        return <div>error</div>
+      }
+
+      const { name, description, thumbnail } = data.character
+
+      const image = `url(${thumbnail.url})`
+
+      return (
+        <div className="CharacterScreen__wrapper">
+          <div className="CharacterScreen__firstLine">
+            <span>close</span>
+          </div>
+          <div className="CharacterScreen__content">
+            <div className="CharacterScreen__content--text">
+              <h1>{name}</h1>
+              <p>{description}</p>
+            </div>
+            <div>
+                <div className="CharacterScreen__image" style={{backgroundImage: image}}></div>
+            </div>
+          </div>
+          <div className="CharacterScreen__bottom">
+            <p>Data provides by Marvel API</p>
+          </div>
+        </div>
+      )
+    }
+  }
+  </Query>
 )
-
 export default CharacterScreen
--- frontend/src/components/CharacterBox.jsx
+++ frontend/src/components/CharacterBox.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ImageCharacter from './ImageCharacter'
 import NameCharacter from './NameCharacter'
+import { Link } from 'react-router-dom';
 
 export default ({ character }) => {
 
-  const { name, thumbnail } = character;
+  const { id, name, thumbnail } = character;
 
   return (
-    <div className="character-box">
-      <NameCharacter name={name} />
-      <ImageCharacter src={thumbnail.url} />
-    </div>
+    <Link to={`/characters/${id}`}>
+      <div className="character-box">
+        <NameCharacter name={name} />
+        <ImageCharacter src={thumbnail.url} />
+      </div>
+    </Link>
   )
 }
