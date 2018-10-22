1cc997bfb8aeaff212f2a8c4f248359f20bb84b7
Mateus Gabi <mateusgabimoreira@hotmail.com>
2018-10-21 20:03:57 -0400

    refactor(frontend): main message translated to english

--- frontend/src/Screens/Home.jsx
+++ frontend/src/Screens/Home.jsx
@@ -9,7 +9,8 @@ class Home extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h3>Você conhece todos os personagens?</h3>
+          <h3>Do you know every Characters?</h3>
+          <h5>Let's begin</h5>
           <i class="material-icons arrow_down">
             arrow_downward
           </i>
